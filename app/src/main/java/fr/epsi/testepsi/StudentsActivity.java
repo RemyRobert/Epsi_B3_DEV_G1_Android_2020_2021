b07533741da9840138b184f2fa2b33fce41a2442
DJEMAM Nidal <nidal_djemam@hotmail.com>
2021-01-26 10:53:42 +0100

    add data

--- app/src/main/java/fr/epsi/testepsi/StudentsActivity.java
+++ app/src/main/java/fr/epsi/testepsi/StudentsActivity.java
@@ -2,6 +2,13 @@ package fr.epsi.testepsi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class StudentsActivity extends EpsiActivity {
 
@@ -9,12 +16,26 @@ public class StudentsActivity extends EpsiActivity {
         Intent intent=new Intent(activity,StudentsActivity.class);
         activity.startActivity(intent);
     }
-
+    private ArrayList<Student> students;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students);
         setTitle("Students");
         showBack();
+
+        try {
+            students=new ArrayList<>();
+            JSONObject jsonObject= new JSONObject(Data.allData);
+            JSONArray jsonArray = jsonObject.getJSONArray("items");
+            for(int i=0;i<jsonArray.length();i++){
+                Student student=new Student(jsonArray.getJSONObject(i));
+                students.add(student);
+            }
+            Log.d("Tag","students : "+students.size());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
